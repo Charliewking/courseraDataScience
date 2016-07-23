f1bbc5d4092680bdcace4e6cd4aee7717ea47569
charlieking <charliewking@outlook.com>
2016-07-23 12:02:43 -0700

    Pushing Some new data to the repo
    For Science.

---
+++ Scripts/Demo1.R
@@ -0,0 +1,14 @@
+# Create a data frame
+df <- data.frame(
+  Name = c("a","b","c"),
+  Value = c(1, 2, 3)
+)
+
+# Displaye the data frame
+print(df)
+
+#plot the data frame using default parameters
+plot(df)
+
+#plot(df$Name, df$Value)
+
